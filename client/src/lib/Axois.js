58531f1449b761c63892fe31ab63c27f8f29f1da
Fenetroba <fenetroba700@gmail.com>
2025-08-10 00:08:16 +0300

    Update Axois.js

--- client/src/lib/Axois.js
+++ client/src/lib/Axois.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
  const instance = axios.create({
 
-  baseURL: "http://localhost:5000/api" || "https://friend-chat-i9ge.onrender.com/api",
+  baseURL:"https://friend-chat-i9ge.onrender.com/api",
   withCredentials: true
 });
 export default instance
